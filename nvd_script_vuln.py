d5c2772e788e284a0c9748288cb2f5df48b6fadd
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2023-09-07 18:16:57 +0530

    Update nvd_script_vuln.py

--- nvd_script_vuln.py
+++ nvd_script_vuln.py
@@ -6,18 +6,27 @@ NVD_URL = 'https://services.nvd.nist.gov/rest/json/cves/1.0'
 def read_input_excel(file_name):
     return pd.read_excel(file_name)
 
-def fetch_vulnerabilities_from_nvd(component, version_prefix):
+def fetch_vulnerabilities_from_nvd(component, version_prefix=None):
     vulnerabilities = []
     cpe_entries = []
     seen_vulnerabilities = set()  # Track seen (component, version, cve) combos
     seen_cpe_entries = set()      # Track seen CPEs for uniqueness
     results_per_page = 2000
     start_index = 0
-    params = {
-        'cpeMatchString': f'cpe:2.3:*:*:{component}:{version_prefix}*:*:*:*:*:*:*',
-        'resultsPerPage': results_per_page,
-        'startIndex': start_index
-    }
+    
+    # Adjust CPE matching string based on the version_prefix availability
+    if version_prefix:
+        params = {
+            'cpeMatchString': f'cpe:2.3:*:*:{component}:{version_prefix}*:*:*:*:*:*:*',
+            'resultsPerPage': results_per_page,
+            'startIndex': start_index
+        }
+    else:
+        params = {
+            'cpeMatchString': f'cpe:2.3:*:*:{component}:*:*:*:*:*:*:*',
+            'resultsPerPage': results_per_page,
+            'startIndex': start_index
+        }
 
     while True:
         response = requests.get(NVD_URL, params=params)
@@ -39,7 +48,7 @@ def fetch_vulnerabilities_from_nvd(component, version_prefix):
                 
                 vulnerabilities.append({
                     'component': component,
-                    'version': version_prefix,
+                    'version': version_prefix if version_prefix else 'All',
                     'cve': cve_id,
                     'description': cve_data['description']['description_data'][0]['value'],
                     'cvss': base_metric_data.get('cvssV3', {}).get('baseScore', 'N/A'),
@@ -54,11 +63,10 @@ def fetch_vulnerabilities_from_nvd(component, version_prefix):
                         cpe_uri = cpe_data['cpe23Uri']
                         if cpe_uri not in seen_cpe_entries:
                             seen_cpe_entries.add(cpe_uri)
-                            # Extracting the component name from the CPE string
                             cpe_component_name = cpe_uri.split(':')[4]
                             cpe_entries.append({
                                 'component': component,
-                                'version': version_prefix,
+                                'version': version_prefix if version_prefix else 'All',
                                 'cpe': cpe_uri,
                                 'cpe_component_name': cpe_component_name
                             })
@@ -70,12 +78,6 @@ def fetch_vulnerabilities_from_nvd(component, version_prefix):
         params['startIndex'] = start_index
 
     return vulnerabilities, cpe_entries
-
-
-
-
-
-
 
 def write_to_excel(vulnerabilities_data, cpe_data, output_file_name):
     with pd.ExcelWriter(output_file_name) as writer:
@@ -92,7 +94,9 @@ def main():
     all_cpe_entries = []
 
     for index, row in components.iterrows():
-        vulnerabilities, cpe_entries = fetch_vulnerabilities_from_nvd(row['component'], row['version'])
+        component = row['component']
+        version = row.get('version', None)  # Default to None if version isn't present
+        vulnerabilities, cpe_entries = fetch_vulnerabilities_from_nvd(component, version)
         all_vulnerabilities.extend(vulnerabilities)
         all_cpe_entries.extend(cpe_entries)
 
